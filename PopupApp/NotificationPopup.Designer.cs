f2559c14c46aa20d3088ea4979a41908e4a4e6f9
Prakash <98660007+prakash-kathiresan@users.noreply.github.com>
2024-02-21 20:32:54 +0530

    Add project files.

---
+++ PopupApp/NotificationPopup.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PopupApp
+{
+    partial class NotificationPopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationPopup));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.snoozeBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = global::PopupApp.Properties.Resources.Bristlecone_Logo_CMYK_Black;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Font = new System.Drawing.Font("Eras Demi ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopBtn.Location = new System.Drawing.Point(99, 198);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(98, 33);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Font = new System.Drawing.Font("Eras Demi ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHide.Location = new System.Drawing.Point(168, 79);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(98, 33);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Eras Demi ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(168, 138);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(98, 33);
+            this.startBtn.TabIndex = 3;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // snoozeBtn
+            // 
+            this.snoozeBtn.Font = new System.Drawing.Font("Eras Demi ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snoozeBtn.Location = new System.Drawing.Point(231, 198);
+            this.snoozeBtn.Name = "snoozeBtn";
+            this.snoozeBtn.Size = new System.Drawing.Size(98, 33);
+            this.snoozeBtn.TabIndex = 4;
+            this.snoozeBtn.Text = "Snooze";
+            this.snoozeBtn.UseVisualStyleBackColor = true;
+            this.snoozeBtn.Click += new System.EventHandler(this.snoozeBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(203, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "or";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IntegralHeight = false;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox2.SelectedItem = "1";
+            this.comboBox2.Location = new System.Drawing.Point(335, 206);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(48, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(389, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Hour";
+            // 
+            // NotificationPopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 288);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.snoozeBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "NotificationPopup";
+            this.Text = "Notification App";
+            this.Load += new System.EventHandler(this.NotificationPopup_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button snoozeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PopupApp/NotificationPopup.cs
@@ -0,0 +1,151 @@
+﻿using PopupApp.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PopupApp
+{
+    public partial class NotificationPopup : Form
+    {
+        public NotificationPopup()
+        {
+            InitializeComponent();
+            //MaximizeBox = false;
+        }
+
+        private string connectionString = @"Data Source=BLR-3ZY31F3\SQLEXPRESS;Initial Catalog=SignalRdb;Integrated Security=True";
+        private string msgId = string.Empty;
+        private string msgTitle = string.Empty;
+        private string msgMessage = string.Empty;
+        private string oldMsgId = "";
+        private bool stopCn = false;
+        private void NotificationPopup_Load(object sender, EventArgs e)
+        {
+            SqlClientPermission sqlClientPermission = new SqlClientPermission(System.Security.Permissions.PermissionState.Unrestricted);
+            sqlClientPermission.Demand();
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            snoozeBtn.Enabled = true;
+            comboBox2.Enabled = true;
+            LoadData();
+
+        }
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            this.btnHide_Click(null, null);
+        }
+        private void hide()
+        {
+            this.Visible = false;
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon(@"C:\Users\Prakash.K.CORPBRISTLECONE\Downloads\notification.ico");
+            trayIcon.Visible = true;
+            trayIcon.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+        }
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            var thisIcon = (NotifyIcon)sender;
+            thisIcon.Visible = false;
+            thisIcon.Dispose();
+        }
+        void LoadData()
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                if (cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+                if (stopCn == true)
+                {
+                    cn.Close();
+                    SqlDependency.Stop(connectionString);
+                }
+                SqlCommand cmd = new SqlCommand("SELECT TOP 1 * FROM POPUPAPP ORDER BY MESSAGEDATE DESC", cn);
+                cmd.Notification = null;
+                SqlDependency.Start(connectionString);
+                SqlDependency sqlDependency = new SqlDependency(cmd);
+                sqlDependency.OnChange += new OnChangeEventHandler(OnDependencyChange);
+                DataTable dt = new DataTable("MessageTable");
+                dt.Load(cmd.ExecuteReader());
+                if (dt.Rows.Count > 0)
+                {
+                    msgId = dt.Rows[0]["MESSAGEID"].ToString();
+                    msgTitle = dt.Rows[0]["TITLE"].ToString();
+                    msgMessage = dt.Rows[0]["MESSAGE"].ToString();
+                }
+                if (msgId != oldMsgId && oldMsgId != "")
+                {
+                    string message = msgMessage;
+                    string title = msgTitle;
+                    MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                oldMsgId = msgId;
+            }
+        }
+        delegate void UpdateData();
+        public void OnDependencyChange(object sender, SqlNotificationEventArgs e)
+        {
+            SqlDependency sqlDependency = sender as SqlDependency;
+            sqlDependency.OnChange -= OnDependencyChange;
+            UpdateData updateData = new UpdateData(LoadData);
+            this.Invoke(updateData, null);
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            stopCn = true;
+            startBtn.Enabled = true;
+            stopBtn.Enabled = false;
+            snoozeBtn.Enabled = false;
+            comboBox2.Enabled = false;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            hide();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            stopCn = false;
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            snoozeBtn.Enabled = true;
+            comboBox2.Enabled = true;
+            LoadData();
+        }
+
+        private async void snoozeBtn_Click(object sender, EventArgs e)
+        {
+            int val = Convert.ToInt16(comboBox2.SelectedItem);
+            int hourDelay = val * 60 * 60000;
+            stopCn = true;
+            startBtn.Enabled = true;
+            stopBtn.Enabled = false;
+            snoozeBtn.Enabled = false;
+            comboBox2.Enabled = false;
+            hide();
+            //await Task.Delay(10000);
+            await PutTaskDelay(hourDelay);
+            startBtn_Click(null, null);
+        }
+        async Task PutTaskDelay(int snoozeDelay)
+        {
+            await Task.Delay(snoozeDelay);
+        }
+    }
+}
